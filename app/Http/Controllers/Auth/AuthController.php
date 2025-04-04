c7383a787ad321f607e5b5e9bab9575e8a4fbb01
zobirofkir <zobirofkir19@gmail.com>
2025-04-04 14:56:38 +0000

    documented auth controller

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -13,21 +13,43 @@ use App\Services\Facades\AuthFacade;
 
 class AuthController extends Controller implements AuthConstructor
 {
+    /**
+     * Register new user
+     *
+     * @param RegisterRequest $request
+     * @return RegisterResource
+     */
     public function register(RegisterRequest $request): RegisterResource
     {
         return AuthFacade::register($request);
     }
 
+    /**
+     * Login the user
+     *
+     * @param LoginRequest $request
+     * @return LoginResource
+     */
     public function login(LoginRequest $request): LoginResource
     {
         return AuthFacade::login($request);
     }
 
+    /**
+     * Get current auth user
+     *
+     * @return CurrentAuthUserResource
+     */
     public function currentAuthUser() : CurrentAuthUserResource
     {
         return AuthFacade::currentAuthUser();
     }
 
+    /**
+     * Logout the user
+     *
+     * @return boolean
+     */
     public function logout() : bool
     {
         return AuthFacade::logout();
