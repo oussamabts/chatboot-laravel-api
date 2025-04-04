bc6531c4d8958f39ff87b29e2e3565f239adc866
zobirofkir <zobirofkir19@gmail.com>
2025-04-04 21:34:18 +0000

    create schema with relation sheep for messages

---
+++ app/Models/Message.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'message',
+        'response'
+    ];
+
+    /**
+     * Get the user that owns the Message
+     *
+     * @return BelongsTo
+     */
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
