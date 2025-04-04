19ce81ab0261a908dd91a35b2da397114fb6d2a8
zobirofkir <zobirofkir19@gmail.com>
2025-04-04 21:51:13 +0000

    create message resource logique to return the data as a resources

--- app/Http/Resources/MessageResource.php
+++ app/Http/Resources/MessageResource.php
@@ -14,6 +14,10 @@ class MessageResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'message' => $this->message,
+            'response' => $this->response
+        ];
     }
 }
