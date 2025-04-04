6d362e378c3bf1ce84eb7af2e793e54d3b95ba8f
zobirofkir <zobirofkir19@gmail.com>
2025-04-04 08:51:13 +0000

    create login controller and request and resource

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Resources\Auth\LoginResource;
 use App\Http\Resources\Auth\RegisterResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -15,5 +18,19 @@ class AuthController extends Controller
         return RegisterResource::make(
             User::create($request->validated())
         );
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        $user = User::where("email" ,$validatedData["email"])->first();
+
+        if (!$user && !Hash::check($validatedData['password'], $user->password))
+        {
+            abort(401);
+        }
+
+        return LoginResource::make($user);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function accessToken() : string
+    {
+        return $this->createToken('accessToken')->accessToken;
+    }
 }
