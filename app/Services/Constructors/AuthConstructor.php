1ee1cb6227308ad584c5d7d85b4f22ecc8c7e52a
zobirofkir <zobirofkir19@gmail.com>
2025-04-04 14:14:27 +0000

    create auth constructor

--- app/Services/Constructors/AuthConstructor.php
+++ app/Services/Constructors/AuthConstructor.php
@@ -2,7 +2,41 @@
 
 namespace App\Services\Constructors;
 
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Resources\Auth\LoginResource;
+use App\Http\Resources\Auth\RegisterResource;
+use App\Http\Resources\CurrentAuthUserResource;
+
 interface AuthConstructor
 {
-    //
+    /**
+     * Register new user
+     *
+     * @param RegisterRequest $request
+     * @return RegisterResource
+     */
+    public function register(RegisterRequest $request) : RegisterResource;
+
+    /**
+     * Login the user
+     *
+     * @param LoginRequest $request
+     * @return LoginResource
+     */
+    public function login(LoginRequest $request) : LoginResource;
+
+    /**
+     * Get current authenticated user
+     *
+     * @return CurrentAuthUserResource
+     */
+    public function currentAuthUser() : CurrentAuthUserResource;
+
+    /**
+     * logout current authenticated user
+     *
+     * @return boolean
+     */
+    public function logout() : bool;
 }
